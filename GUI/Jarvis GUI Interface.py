38d184f60368dac2b749356d41ce42a7dded20b6
Harshit <harshitkumarsaini88@gmail.com>
2020-10-29 16:50:17 +0530

    modified jarvies with cool widgets

--- GUI/Jarvis GUI Interface.py
+++ GUI/Jarvis GUI Interface.py
@@ -13,7 +13,7 @@ h=root.winfo_screenheight()
 w=root.winfo_screenwidth()
 
 #Open Background Iron Man Jarvis Image
-pic=Image.open("iron-man.jpg")
+pic=Image.open("background.png")
 resized=pic.resize((w,h),Image.ANTIALIAS)
 
 #Set Background image
@@ -22,24 +22,46 @@ lab=Label(root,image=img_backg)
 lab.pack()
 
 
+
+
 #Jarvis starting rotating function
-a=0
+number=0
+po="lo"
 def hello():
-    global a
+    global number
     global start_button
     global start_jarvis_img
-    a = a - 5
-    z = start_jarvis.rotate(a)
+    global po
+
+    number = number - 10
+    z = resized_start_jarvis.rotate(number)
     start_jarvis_img = ImageTk.PhotoImage(z)
     start_button.config(image=start_jarvis_img,state=DISABLED)
-    start_button.after(50, hello)
+    stop_button.config(state=NORMAL)
+    po=start_button.after(50, hello)
 
-start_jarvis = Image.open("start-jarvis.png")
-start_jarvis_img = ImageTk.PhotoImage(start_jarvis)
+def stop_spin():
+    global start_button
+    root.after_cancel(po)
+    start_button.config(image=lop,state=NORMAL,bg="#090909")
+    stop_button.config(state=DISABLED)
 
-start_button=Button(root,image=start_jarvis_img, command=hello, bd=10, bg='Black')
-start_button.place(x=30,y=40)
 
+
+
+
+start_jarvis = Image.open("123.png")
+resized_start_jarvis = start_jarvis.resize((220,220),Image.ANTIALIAS)
+start_jarvis_img = ImageTk.PhotoImage(resized_start_jarvis)
+lop=start_jarvis_img
+
+start_button=Button(root,image=start_jarvis_img,bg="#090909", command=hello,bd=0,activebackground="#090909")
+start_button.place(x=100,y=420)
+
+#Creating Stop Image and Button
+stop_image = ImageTk.PhotoImage(Image.open("stop.png"))
+stop_button = Button(root,image=stop_image,command=stop_spin,bg="#090909",bd=0,state=DISABLED,activebackground="#090909")
+stop_button.place(x=1050,y=420)
 
 root.mainloop()
 
